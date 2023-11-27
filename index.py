ec7d0e6fae82b35c08c11dac944f3424393e8dd2
Gabe Macri <gabemacru@gmail.com>
2023-11-27 16:48:06 +0000

    first

---
+++ index.py
@@ -0,0 +1,18 @@
+def one()
+    print("Hello, World!")
+
+def two()
+    if 5 > 2:
+  print("Five is greater than two!")
+
+x = 5
+y = "John"
+
+def three()
+    print(x)
+    print(y)
+
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+
